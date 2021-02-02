bb623847613c8c746bdd0dc761070fafa063fa34
stepas-dev <stepas.apple@gmail.com>
2021-02-02 21:34:33 +0200

    sending emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 const User = require('../models/user');
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: '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',
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let errorMessage = req.flash('error');
@@ -88,6 +97,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(() => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'stepas.apple@gmail.com',
+            subject: 'Signup succeeeded!',
+            html: '<h1>You successfully signed up!</h1>',
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
